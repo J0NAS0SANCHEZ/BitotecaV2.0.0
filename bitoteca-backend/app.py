c8e898289fc23501ef026d87032b96f0297244d9
J0NAS0SANCHEZ <128201674+J0NAS0SANCHEZ@users.noreply.github.com>
2025-05-03 14:41:48 -0600

    Add files via upload

--- bitoteca-backend/app.py
+++ bitoteca-backend/app.py
@@ -7,5 +7,11 @@ app.register_blueprint(email_bp)
 
 # ... aqui va otros blueprints como /books, etc.
 
+@app.route("/test-sendgrid")
+def test_sendgrid_key():
+    key = os.getenv("SENDGRID_API_KEY")
+    return f"SendGrid key visible?: {key is not None}"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
